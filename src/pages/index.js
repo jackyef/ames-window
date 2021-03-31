9fa0c2358030be905bced7304038137c73c6d8ef
jackyef <jacky.efendi1@gmail.com>
2021-03-31 18:21:57 +0700

    commit dlu

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,21 +17,21 @@ function AmesWindow(props) {
 
   return (
     <group ref={group} scale={0.8} {...props}>
-      <mesh position={[-2.88, 0, 0]}>
-        <boxGeometry args={[thickness, 1.7, thickness]} />
-        <meshStandardMaterial color={color} />
-      </mesh>
       <mesh position={[0, 1.3, 0]} rotation-z={0.175}>
         <boxGeometry args={[6, thickness, thickness]} />
         <meshStandardMaterial color={color} />
       </mesh>
-      <mesh position={[2.87, 0, 0]}>
-        <boxGeometry args={[thickness, 3.79, thickness]} />
+      <mesh position={[0, -1.3, 0]} rotation-z={-0.175}>
+        <boxGeometry args={[6, thickness, thickness]} />
         <meshStandardMaterial color={color} />
       </mesh>
 
-      <mesh position={[0, -1.3, 0]} rotation-z={-0.175}>
-        <boxGeometry args={[6, thickness, thickness]} />
+      <mesh position={[-2.88, 0, 0]}>
+        <boxGeometry args={[thickness, 1.7, thickness]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+      <mesh position={[2.87, 0, 0]}>
+        <boxGeometry args={[thickness, 3.79, thickness]} />
         <meshStandardMaterial color={color} />
       </mesh>
 
@@ -56,7 +56,9 @@ function AmesWindow(props) {
 function AmesWindow2(props) {
   const group = useRef();
   const thickness = 0.2;
-  const color = "red";
+  const minRadius = 0.15 / Math.sqrt(2);
+  const maxRadius = 0.25 / Math.sqrt(2);
+  const color = "royalblue";
 
   // Rotate the group every frame, this is outside of React without overhead
   useFrame(() => {
@@ -64,9 +66,52 @@ function AmesWindow2(props) {
   });
 
   return (
-    <group ref={group}>
+    <group ref={group} scale={0.8}>
+      {/* Top-border */}
+      <mesh position={[0, 1.4, 0]} rotation-z={1.73}>
+        <cylinderGeometry
+          args={[minRadius, maxRadius, 6, 16, 1]}
+        />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      {/* Top-border */}
+      <mesh position={[0, -1.4, 0]} rotation-z={-1.73}>
+        <cylinderGeometry
+          args={[maxRadius, minRadius, 6, 16, 1]}
+        />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      {/* Left-border */}
+      <mesh position={[-2.88, 0, 0]}>
+        <cylinderGeometry
+          args={[minRadius, minRadius, 2, 16, 1]}
+        />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      {/* Right-border */}
+      <mesh position={[2.79, 0, 0]}>
+        <cylinderGeometry
+          args={[maxRadius, maxRadius, 3.8, 16, 1]}
+        />
+        {/* <boxGeometry args={[thickness * 1.66, 3.79, thickness * 1.66]} /> */}
+        <meshStandardMaterial color={color} />
+      </mesh>
+
       <mesh position={[0, 0, 0]}>
-        <convexGeometry args={[thickness, 1.7, thickness]} />
+        <boxGeometry args={[5.8, thickness, thickness]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      <mesh position={[-1.3, 0, 0]} rotation-z={(Math.PI * 1) / 2}>
+        <boxGeometry args={[2.2, thickness, thickness]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      <mesh position={[0.7, 0, 0]} rotation-z={(Math.PI * 1) / 2}>
+        <boxGeometry args={[2.9, thickness, thickness]} />
         <meshStandardMaterial color={color} />
       </mesh>
     </group>
@@ -78,18 +123,20 @@ export default function Home() {
     <>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>Ames Window @react-three/fiber</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+          <h1 className={styles.title}>Ames Window Illusion</h1>
 
           <Canvas className="canvas" orthographic>
             <ambientLight />
-            {/* <spotLight position={[10, 10, 10]} angle={Math.PI * 1/2} penumbra={1} /> */}
+            <spotLight
+              position={[10, 10, 10]}
+              angle={(Math.PI * 1) / 2}
+              penumbra={1}
+            />
             <AmesWindow position={[0, 0, 0]} />
 
             {/* <AmesWindow2 position={[0, 0, 0]} /> */}
